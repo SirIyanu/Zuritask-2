8682342cbf6b5c337a43b0999fe1d0a4485b2571
Sir Iyanu <iyolawoye@gmail.com>
2022-06-03 21:04:36 +0100

    second commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+
+while True:
+    choices =["R", "P", "S"]
+    
+    CPU = random.choice(choices)
+    player = None
+    
+    while player not in choices:
+        player = input("R, P or S?: ")
+    if player == CPU:
+            print("CPU: ",CPU)
+            print("player: ",player)
+            print("Tie!")
+    elif player == "R":
+        if CPU == "P":
+            print("CPU: ",CPU)
+            print("player: ",player)
+            print("You lose!") 
+        if CPU == "S":
+            print("CPU: ",CPU)
+            print("player: ",player)
+            print("Winner!")
+    elif player == "S":
+        if CPU == "R":
+            print("CPU: ",CPU)
+            print("player: ",player)
+            print("You lose!") 
+        if CPU == "P":
+            print("CPU: ",CPU)
+            print("player: ",player)
+            print("Winner!") 
+    elif player == "P":
+        if CPU == "S":
+            print("CPU: ",CPU)
+            print("player: ",player)
+            print("You lose!") 
+        if CPU == "R":
+            print("CPU: ",CPU)
+            print("player: ",player)
+            print("Winner!")
+    
+    play_again = input("Play again? (yes/no): ").lower()
+
+    if play_again != "yes": 
+        break
+print("Bye!")
+
+
+             
+
